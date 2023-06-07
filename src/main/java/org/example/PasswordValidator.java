a38237992fc0af4d28c5b5f8097c30052a1bac61
cananozyol <canan.oezyol@live.de>
2023-06-07 15:50:52 +0200

    Initial commit

---
+++ src/main/java/org/example/PasswordValidator.java
@@ -0,0 +1,61 @@
+package org.example;
+import java.util.Scanner;
+
+public class PasswordValidator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your password: ");
+        String password = scanner.nextLine();
+        scanner.close();
+        System.out.println(passwordValidation(password));
+    }
+    public static boolean passwordValidation(String password){
+        if(passwordLength(password) == true && passwordContainsDigits(password) == true && passwordUpperCaseLetters(password) == passwordDoNotUse(password)){
+            System.out.println("Your password is valid.");
+            return true;}
+        else {
+            return false;
+        }
+    }
+    public static boolean passwordLength(String password) {
+        if (password.length() >= 8) {
+            return true;
+        } else {
+            System.out.println("The password must be at least 8 characters");
+            return false;
+        }
+    }
+
+    public static boolean passwordContainsDigits(String password){
+        char[] checkForNumbers = password.toCharArray();
+        for (char n: checkForNumbers){
+            if (Character.isDigit(n)){
+                return true;
+            }
+        }
+        System.out.println("The password must contain at least one number");
+        return false;
+    }
+
+    public static boolean passwordUpperCaseLetters(String password) {
+        char[] checkForUpperCase = password.toCharArray();
+        for (char uc : checkForUpperCase) {
+            if (Character.isUpperCase(uc)) {
+                return true;
+            }
+        }
+        System.out.println("The password must contain at least one uppercase Letter");
+        return false;
+    }
+    public static boolean passwordDoNotUse(String password) {
+        String str1 = "Password123";
+        String str2 = "P1234567";
+
+        if (password.equals(str1) || password.equals(str2)) {
+            System.out.println("The Password is not secure enough.");
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/PasswordValidatorTest.java
@@ -0,0 +1,38 @@
+import org.example.PasswordValidator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PasswordValidatorTest {
+    @Test
+    void isPasswordAtLeast8_pwEquals7(){
+        assertFalse(PasswordValidator.passwordLength("pw34567"));
+    }
+    @Test
+    void isPasswordAtLeast8_pwEquals8(){
+        assertTrue(PasswordValidator.passwordLength("pw345678"));
+    }
+    @Test
+    void containsDigit_withDigit(){
+        assertTrue(PasswordValidator.passwordContainsDigits("password123"));
+    }
+    @Test
+    void containsDigit_withoutDigit(){
+        assertFalse(PasswordValidator.passwordContainsDigits("password"));
+    }
+    @Test
+    void containsUpperCase_withUpperCase(){
+        assertTrue(PasswordValidator.passwordUpperCaseLetters("Password"));
+    }
+    @Test
+    void containsUpperCase_withoutUpperCase(){
+        assertFalse(PasswordValidator.passwordUpperCaseLetters("password"));
+    }
+    @Test
+    void isPasswordEasy_pwIsEasy(){
+        assertFalse(PasswordValidator.passwordDoNotUse("Password123"));
+    }
+    @Test
+    void isPasswordEasy_pwIsNotEasy(){
+        assertTrue(PasswordValidator.passwordDoNotUse("Password12"));
+    }
+}
